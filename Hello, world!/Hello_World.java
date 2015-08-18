dd079494b8d4c82df33f2ae197ae4d052c0b0ae4
hannahersey <16herseyh65@millshighsch.org>
2015-08-18 09:21:30 -0700

    Hi
    Cool

---
+++ Hello, world!/Hello_World.java
@@ -0,0 +1,15 @@
+
+/**
+ * First Java code! Will do a simple println.
+ * 
+ * Hannah Hersey 
+ * 8/18/2015
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world! Love, Hannah");
+
+    }
+}
