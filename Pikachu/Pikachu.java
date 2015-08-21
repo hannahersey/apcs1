5f8c5c834f9d4b2254cae56138bc593487850746
Hannah Hersey <16herseyh65@millshighsch.org>
2015-08-21 08:44:24 -0700

    Hi
    Hello again!

---
+++ Pikachu/Pikachu.java
@@ -0,0 +1,19 @@
+
+/**
+ * Pikachu welcomes you to the world of Pokemon!
+ * 
+ * Hannah
+ * 8/21/15
+ */
+public class Pikachu
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Pikachu welcomes you to the world of Pokemon!");
+        System.out.println();
+        System.out.println("\t (\\__/)");
+        System.out.println("\t (o^.^)");
+        System.out.println("\tz(\")(\"0");
+    }
+    
+}
