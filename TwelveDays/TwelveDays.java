9393c73e8f2f2f6cbe86743f2b2371f41440817e
hannahersey <16herseyh65@millshighsch.org>
2015-08-27 14:31:01 -0700

    Twelve Days
    of hell

---
+++ TwelveDays/TwelveDays.java
@@ -0,0 +1,190 @@
+
+/**
+ * This was miserable..
+ * 
+ * Hannah Hersey 
+ * 8/27/15
+ */
+public class TwelveDays
+{
+    public static void main(String[] args) {
+        System.out.println("On the first ");
+        DayLove();
+        Patridge();
+        System.out.println();
+        System.out.println("On the second ");
+        DayLove();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the third ");
+        DayLove();
+        Hen();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the fourth ");
+        DayLove();
+        Bird();
+        Hen();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the fifth ");
+        DayLove();
+        Rings();
+        Bird();
+        Hen();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the sixth ");
+        DayLove();
+        Geese();
+        Rings();
+        Bird();
+        Hen();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the seventh ");
+        DayLove();
+        Swans();
+        Geese();
+        Rings();
+        Bird();
+        Hen();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the eigth ");
+        DayLove();
+        Maids();
+        Swans();
+        Geese();
+        Rings();
+        Bird();
+        Hen();
+        Turtle();
+        System.out.println(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the ninth ");
+        DayLove();
+        Ladies();
+        Maids();
+        Swans();
+        Geese();
+        Rings();
+        Bird();
+        Hen();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the tenth ");
+        DayLove();
+        Lords();
+        Ladies();
+        Maids();
+        Swans();
+        Geese();
+        Rings();
+        Bird();
+        Hen();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the eleventh ");
+        DayLove();
+        Pipers();
+        Lords();
+        Ladies();
+        Maids();
+        Swans();
+        Geese();
+        Rings();
+        Bird();
+        Hen();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+        System.out.println();
+        System.out.println("On the twelfth ");
+        DayLove();
+        Drummers();
+        Pipers();
+        Lords();
+        Ladies();
+        Maids();
+        Swans();
+        Geese();
+        Rings();
+        Bird();
+        Hen();
+        Turtle();
+        System.out.print(" and ");
+        Patridge();
+    }
+    
+    public static void DayLove() {
+        System.out.println("day of Chistmas \n My true love gave to me,");
+    }
+    
+        public static void Drummers(){
+        System.out.println("Twelve Drummers Drumming,");
+    }
+    
+        public static void Pipers(){
+        System.out.println("Eleven Pipers Piping,");
+    }
+    
+        public static void Lords(){
+        System.out.println("Ten Lords a Leaping,");
+    }
+    
+        public static void Ladies(){
+        System.out.println("Nine Ladies Dancing,");
+    }
+    
+        public static void Maids(){
+        System.out.println("Eight Maids a Milking,");
+    }
+    
+        public static void Swans(){
+        System.out.println("Seven Swans a Swimming,");
+    }
+    
+        public static void Geese(){
+        System.out.println("Six Geese a Laying,");
+    }
+    
+        public static void Rings(){
+        System.out.println("Five Golden Rings,");
+    }
+    
+        public static void Bird(){
+        System.out.println("Four Calling Birds,");
+    }
+    
+        public static void Hen(){
+        System.out.println("Three French Hens,");
+    }
+    
+        public static void Turtle(){
+        System.out.println("Two Turtle Doves,");
+    }
+    
+        public static void Patridge(){
+        System.out.println("A Partridge in a Pear Tree!");
+    }
+    
+}
+        
