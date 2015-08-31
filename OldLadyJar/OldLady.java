5d5a985a2b1234d7d7e31f831c726bc1504ef86d
hannahersey <16herseyh65@millshighsch.org>
2015-08-31 08:45:12 -0700

    Old Lady
    part 2

---
+++ OldLadyJar/OldLady.java
@@ -0,0 +1,71 @@
+
+/**
+ * Really hoping this works! *Double crosses figners#
+ * 
+ * Hannah Hersey
+ * 8/26/15
+ */
+public class OldLady
+{
+   public static void main(String[] args) {
+       there();
+       System.out.println("fly;");
+       idk();
+       there();
+       System.out.println("spider;");
+       System.out.println("That wriggled and wiggled and tiggled inside her;");
+       she();
+       System.out.print(" spider");
+       toCatch();
+       System.out.println(" fly;");
+       idk();
+       there();
+       System.out.println("bird;");
+       System.out.println("How absurd to swallow a bird.");
+       she();
+       System.out.print(" bird");
+       toCatch();
+       System.out.println(" spider;");
+       she();
+       System.out.print(" spider");
+       toCatch();
+       System.out.println(" fly;");
+       idk();
+       there();
+       System.out.println("cat;");
+       System.out.println("Fancy that to swallow a cat!");
+       she();
+       System.out.print(" cat");
+       toCatch();
+       System.out.println(" bird;");
+       she();
+       System.out.print(" bird");
+       toCatch();
+       System.out.println(" spider;");
+       she();
+       System.out.print(" spider");
+       toCatch();
+       System.out.println(" fly;");
+       idk();
+       there();
+       System.out.println("horse...");
+       System.out.println("She's dead of course!");
+    }
+   
+   public static void there() {
+       System.out.print("There was an old lady who swallowed a ");
+    }
+   
+   public static void idk() {
+       System.out.println("I don't know why she swallowed a fly - perhaps she'll die!");
+    }
+   
+   public static void she() {
+       System.out.print("She swallowed the");
+    }
+   
+   public static void toCatch() {
+       System.out.print(" to catch the");
+    }
+    
+}
