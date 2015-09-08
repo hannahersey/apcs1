8c7cde9f8adf2eff63745fe1b6098b4fd3c646d6
hannahersey <16herseyh65@millshighsch.org>
2015-09-08 09:22:46 -0700

    okay

---
+++ Temperture/FahrenheitToCelcius.java
@@ -0,0 +1,13 @@
+
+/**
+ * Convert Fahrenheit to celsius
+ * 
+ * Hannah Hersey
+ * 8/9/15
+ */
+public class FahrenheitToCelcius
+{
+    double fahrenheit = 0.0;
+    double celcius = 5.0/9.0*(fahrenheit - 32.0);
+    System.out.println(celcius);
+}
